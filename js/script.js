eb98710d32ba533035987dbb8ab9d6319a56c1d0
Lucas-Osmond <124389349+Lucas-Osmond@users.noreply.github.com>
2023-03-21 17:08:54 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked () {
+  // input
+  const streetNumber=document.getElementById("first-name").value
+  const streetName=parseInt(document.getElementById("age-entered").value) 
+  
+  // output
+  document.getElementById("address").innerHTML=
+  "Your address is: " + streetNumber + " "+ streetName + "."
+}
